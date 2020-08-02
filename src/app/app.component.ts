cea9485d270cc68b9a4421a0477181ea7188eff6
Leonardo Souza Lima de Moura <leodemoura@gmail.com>
2020-08-01 22:57:57 -0500

    changes up to step 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { Satellite } from './satellite';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
   title = 'orbit-report';
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          // TODO: loop over satellites
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+ }
 }
