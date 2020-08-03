b7b4a543fce2c3c85a503420ed432f03742ad416
Leonardo Souza Lima de Moura <leodemoura@gmail.com>
2020-08-02 21:23:13 -0500

    up to step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Satellite } from './satellite';
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +9,29 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'orbit-report';
-  constructor() {
-    this.sourceList = [];
-    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
- 
-    window.fetch(satellitesUrl).then(function(response) {
-       response.json().then(function(data) {
- 
-          let fetchedSatellites = data.satellites;
-          // TODO: loop over satellites
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
-       }.bind(this));
-    }.bind(this));
- 
- }
+   title = 'orbit-report';
+   sourceList: Satellite[];
+
+   constructor() {
+      this.sourceList = [];
+      let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+      window.fetch(satellitesUrl).then(function (response) {
+         response.json().then(function (data) {
+
+            let fetchedSatellites = data.satellites;
+
+            // TODO: loop over satellites
+            for (let i = 0; i < fetchedSatellites.length; i++) {
+               let satellite = new Satellite(fetchedSatellites[i].name,
+                  fetchedSatellites[i].type, fetchedSatellites[i].launchDate,
+                  fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+               this.sourceList.push(satellite);
+            }
+
+
+         }.bind(this));
+      }.bind(this));
+
+   }
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,5 +1,3 @@
-import { runInThisContext } from "vm";
-
 export class Satellite {
     name: string;
     orbitType: string;
